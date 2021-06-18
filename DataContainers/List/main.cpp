5c8dd5f039bd8e922112b4614c01857b599f0568
Bato_gt05@itstepru.onmicrosoft.com <Bato_gt05@itstepru.onmicrosoft.com>
2021-06-18 18:21:07 +0300

    BaseIterator_methods_out_of_class

--- DataContainers/List/main.cpp
+++ DataContainers/List/main.cpp
@@ -32,36 +32,12 @@ class List
 	protected:
 		Element* Temp;
 	public:
-		BaseIterator(Element* Temp = nullptr) :Temp(Temp)
-		{
-
-#ifdef DEBUG
-			cout << "BIConstructor:\t\t" << this << endl;
-#endif // DEBUG
-
-		}
-		~BaseIterator()
-		{
-#ifdef DEBUG
-			cout << "BIDestructor:\t\t" << this << endl;
-#endif // DEBUG
-		}
-		bool operator==(const BaseIterator& other)const
-		{
-			return this->Temp == other.Temp;
-		}
-		bool operator!=(const BaseIterator& other)const
-		{
-			return this->Temp != other.Temp;
-		}
-		const T& operator*()const
-		{
-			return Temp->Data;
-		}
-		T& operator*()
-		{
-			return Temp->Data;
-		}
+		BaseIterator(Element* Temp = nullptr);
+		~BaseIterator();
+		bool operator==(const BaseIterator& other)const;
+		bool operator!=(const BaseIterator& other)const;
+		const T& operator*()const;
+		T& operator*();
 
 	};
 public:
@@ -203,6 +179,41 @@ template<typename T>List<T>::Element::~Element()
 #endif // DEBUG
 
 }
+
+template<typename T>
+List<T>::BaseIterator::BaseIterator(Element* Temp ) :Temp(Temp)
+{
+
+#ifdef DEBUG
+	cout << "BIConstructor:\t\t" << this << endl;
+#endif // DEBUG
+
+}
+template<typename T>
+List<T>::BaseIterator::~BaseIterator()
+{
+#ifdef DEBUG
+	cout << "BIDestructor:\t\t" << this << endl;
+#endif // DEBUG
+}
+template<typename T> bool List<T>::BaseIterator::operator==(const BaseIterator& other)const
+{
+	return this->Temp == other.Temp;
+}
+template<typename T>bool List<T>::BaseIterator::operator!=(const BaseIterator& other)const
+{
+	return this->Temp != other.Temp;
+}
+template<typename T>const T& List<T>::BaseIterator::operator*()const
+{
+	return Temp->Data;
+}
+template<typename T>T& List<T>::BaseIterator::operator*()
+{
+	return Temp->Data;
+}
+
+
 
 template<typename T>size_t List<T>::getSize()const
 {
