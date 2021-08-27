1e3c87289ac37f2ce0221cf5740776ec724edc1f
Bato_gt05@itstepru.onmicrosoft.com <Bato_gt05@itstepru.onmicrosoft.com>
2021-08-27 16:06:35 +0300

    ListBox

--- WinAPI/WinAPI/main.cpp
+++ WinAPI/WinAPI/main.cpp
@@ -3,6 +3,15 @@
 
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
+CONST CHAR* LIST_CONTENT[] =
+{
+	"This",
+	"is",
+	"my",
+	"first",
+	"ListBox"
+};
+
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
 {
 	//MessageBox(NULL, "Это мое первое окно, созданное в WinAPI", "Info MessageBox", MB_ICONINFORMATION | MB_YESNOCANCEL); // MB - message box
@@ -18,13 +27,28 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	{
 		HICON hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON1));
 		SendMessage(hwnd, WM_SETICON, 1, (LPARAM)hIcon);
+		HWND hList = GetDlgItem(hwnd, IDC_LIST1);
+		for (int i = 0; i < sizeof(LIST_CONTENT) / sizeof(LIST_CONTENT[0]); i++)
+		{
+			SendMessage(hList, LB_ADDSTRING, 0, (LPARAM)LIST_CONTENT[i]);
+		}
 	}
-		break;
+	break;
 	case WM_COMMAND:
 	{
 		switch (LOWORD(wParam))
 		{
-		case IDOK: MessageBox(NULL, "Была нажата кнопка ОК", "Info", MB_ICONINFORMATION | MB_OK); break;
+		case IDOK:
+		{
+			HWND hList = GetDlgItem(hwnd, IDC_LIST1);
+			int i = SendMessage(hList, LB_GETCURSEL, 0, 0);
+			//MessageBox(NULL, LIST_CONTENT[i], "Вы выбрали ", MB_ICONINFORMATION | MB_OK);
+			CHAR sz_buffer[256] = {};
+			SendMessage(hList, LB_GETTEXT, 0, (LPARAM)sz_buffer);
+			MessageBox(NULL, sz_buffer, "Вы выбрали ", MB_ICONINFORMATION | MB_OK);
+
+			//MessageBox(NULL, "Была нажата кнопка ОК", "Info", MB_ICONINFORMATION | MB_OK); break;
+		}
 		case IDCANCEL: EndDialog(hwnd, 0); break;
 
 		}
