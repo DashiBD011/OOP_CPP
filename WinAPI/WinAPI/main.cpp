01802e020d030f426afbb4d652755a148d0225ee
Bato_gt05@itstepru.onmicrosoft.com <Bato_gt05@itstepru.onmicrosoft.com>
2021-08-27 18:34:29 +0300

    ComboBox

--- WinAPI/WinAPI/main.cpp
+++ WinAPI/WinAPI/main.cpp
@@ -1,6 +1,11 @@
 #include<Windows.h>
 #include"resource.h"
 
+CONST CHAR* cb_content[] =
+{
+	"This","is","my","first","Combo","Box"
+};
+
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
@@ -18,13 +23,33 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	{
 		HICON hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON1));
 		SendMessage(hwnd, WM_SETICON, 1, (LPARAM)hIcon);
+
+		HWND hCombo = GetDlgItem(hwnd, IDC_COMBO1);
+		for (int i = 0; i < sizeof(cb_content) / sizeof(cb_content[0]); i++)
+		{
+			SendMessage(hCombo, CB_ADDSTRING, 0, (LPARAM)cb_content[i]);
+		}
+		
 	}
-		break;
+	break;
 	case WM_COMMAND:
 	{
 		switch (LOWORD(wParam))
 		{
-		case IDOK: MessageBox(NULL, "Была нажата кнопка ОК", "Info", MB_ICONINFORMATION | MB_OK); break;
+		case IDOK:
+		{
+
+			//MessageBox(NULL, "Была нажата кнопка ОК", "Info", MB_ICONINFORMATION | MB_OK); 
+			HWND hCombo = GetDlgItem(hwnd, IDC_COMBO1);
+			INT i = SendMessage(hCombo, CB_GETCURSEL, 0, 0);
+			CHAR sz_buffer[256]{};
+			SendMessage(hCombo, CB_GETLBTEXT, i, (LPARAM)sz_buffer);
+			CHAR sz_msg[256]{};
+			strcpy(sz_msg, "Вы выбрали какой-то пункт: ");
+			strcat(sz_msg, sz_buffer);
+			MessageBox(hwnd, sz_msg, "Info: ", MB_ICONINFORMATION | MB_OK);			
+		}
+		break;
 		case IDCANCEL: EndDialog(hwnd, 0); break;
 
 		}
